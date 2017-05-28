ec32d4834e72eda79e1f9c7bf6e5fa51ec7566f1
Juto Yu <jutkko@gmail.com>
2017-05-28 22:05:55 +0100

    Add test main

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router.Run(":" + port)
+}
