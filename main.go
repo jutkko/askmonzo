1f288d4727a8c63e5220dd2c37eeed50fa65c3c1
Juto Yu <jutkko@gmail.com>
2017-06-05 08:32:13 +0100

    Generate random string for state and accommodate expiry time
    - The server stores the state in memory

--- main.go
+++ main.go
@@ -4,21 +4,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Response struct {
-	AccessToken  string `json:"access_token"`
-	ClientID     string `json:"client_id"`
-	ExpiresIn    int    `json:"expires_in,int"`
-	RefreshToken string `json:"refresh_token"`
-	TokenType    string `json:"token_type"`
-	UserID       string `json:"user_id"`
+var state string
+var expiryTime time.Time
+var authResponse AuthResponse
+
+type AuthResponse struct {
+	AccessToken         string `json:"access_token"`
+	ClientID            string `json:"client_id"`
+	ExpiresIn           int    `json:"expires_in,int"`
+	RefreshToken        string `json:"refresh_token"`
+	TokenType           string `json:"token_type"`
+	UserID              string `json:"user_id"`
+	AuthExpiryTimestamp int64
+}
+
+type BearerResponse struct {
+	Authenticated bool   `json:"authenticated"`
+	ClientID      string `json:"client_id"`
+	UserID        string `json:"user_id"`
 }
 
 func main() {
@@ -51,11 +65,13 @@ func pingHandler(c *gin.Context) {
 }
 
 func authHandlerWrapper(clientID string) func(c *gin.Context) {
+	state = getRandomString()
+
 	return func(c *gin.Context) {
 		link := url.URL{
 			Scheme:   "https",
 			Host:     "auth.getmondo.co.uk",
-			RawQuery: "client_id=" + clientID + "&redirect_uri=https://" + c.Request.Host + "/auth/callback&response_type=code",
+			RawQuery: "client_id=" + clientID + "&redirect_uri=https://pure-oasis-86979.herokuapp.com/auth/callback&response_type=code&state=" + state,
 		}
 		c.Redirect(http.StatusTemporaryRedirect, link.String())
 	}
@@ -63,50 +79,63 @@ func authHandlerWrapper(clientID string) func(c *gin.Context) {
 
 func setAuthCallbackEndpointWrapper(clientID, clientSecret string) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		err := c.Request.ParseForm()
-		if err != nil {
-			panic("Failed to parse form")
-		}
-
 		client := &http.Client{}
+		var resp *http.Response
+		var err error
 
-		authorizationCode := c.Request.Form.Get("code")
+		if authResponse.AuthExpiryTimestamp <= time.Now().Unix() {
 
-		form := url.Values{}
-		form.Add("grant_type", "authorization_code")
-		form.Add("client_id", clientID)
-		form.Add("client_secret", clientSecret)
-		form.Add("redirect_uri", "https://pure-oasis-86979.herokuapp.com/auth/callback")
-		form.Add("code", authorizationCode)
+			err := c.Request.ParseForm()
+			if err != nil {
+				panic("Failed to parse form")
+			}
 
-		req, err := http.NewRequest("POST", "https://api.monzo.com/oauth2/token", strings.NewReader(form.Encode()))
+			authorizationCode := c.Request.Form.Get("code")
+			monzoState := c.Request.Form.Get("state")
+			if monzoState != state {
+				c.JSON(404, gin.H{
+					"Error": "The state does not match, what are you trying to do",
+				})
+				return
+			}
+
+			form := url.Values{}
+			form.Add("grant_type", "authorization_code")
+			form.Add("client_id", clientID)
+			form.Add("client_secret", clientSecret)
+			form.Add("redirect_uri", "https://pure-oasis-86979.herokuapp.com/auth/callback")
+			form.Add("code", authorizationCode)
+
+			req, err := http.NewRequest("POST", "https://api.monzo.com/oauth2/token", strings.NewReader(form.Encode()))
+			if err != nil {
+				panic("Failed to create a new request")
+			}
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+			resp, err = client.Do(req)
+			if err != nil {
+				panic("Failed to create a new request")
+			}
+
+			defer resp.Body.Close()
+			respBody, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic("Failed to create a new request")
+			}
+
+			err = json.Unmarshal(respBody, &authResponse)
+			if err != nil {
+				panic("Failed to unmarshal the response" + err.Error())
+			}
+			authResponse.AuthExpiryTimestamp = time.Now().Unix() + int64(authResponse.ExpiresIn)
+
+		}
+
+		req, err := http.NewRequest("GET", "https://api.monzo.com/ping/whoami", nil)
 		if err != nil {
 			panic("Failed to create a new request")
 		}
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			panic("Failed to create a new request")
-		}
-
-		defer resp.Body.Close()
-		respBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic("Failed to create a new request")
-		}
-
-		var response Response
-		err = json.Unmarshal(respBody, &response)
-		if err != nil {
-			panic("Failed to unmarshal the response" + err.Error())
-		}
-
-		req, err = http.NewRequest("GET", "https://api.monzo.com/ping/whoami", nil)
-		if err != nil {
-			panic("Failed to create a new request")
-		}
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", response.AccessToken))
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", authResponse.AccessToken))
 		req.Header.Set("Content-Type", "application/json")
 
 		resp1, err := client.Do(req)
@@ -114,14 +143,23 @@ func setAuthCallbackEndpointWrapper(clientID, clientSecret string) func(c *gin.C
 			panic(fmt.Sprintf("Failed to create a new request", err))
 		}
 
-		respBody, err = ioutil.ReadAll(resp1.Body)
+		respBody, err := ioutil.ReadAll(resp1.Body)
 		if err != nil {
 			panic("Failed to create a new request")
 		}
 
-		c.JSON(resp.StatusCode, gin.H{
-			"message": string(respBody),
+		var bearerResponse BearerResponse
+		err = json.Unmarshal(respBody, &bearerResponse)
+		if err != nil {
+			panic("Failed to unmarshal the response" + err.Error())
+		}
+
+		c.JSON(resp1.StatusCode, gin.H{
+			"message": "authentication successful",
 		})
+
+		// Successful exchange
+
 	}
 }
 
@@ -132,4 +170,10 @@ func getEnv(v string) string {
 	}
 
 	return env
+}
+
+func getRandomString() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	number := r.Int63()
+	return strconv.FormatInt(number, 10)
 }
