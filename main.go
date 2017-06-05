a061b5a75aca08ff1c1931b10304ec88f22c79b8
Juto Yu <jutkko@gmail.com>
2017-06-05 22:01:19 +0100

    Auto-refresh the token

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func authHandlerWrapper(clientID string) func(c *gin.Context) {
 		link := url.URL{
 			Scheme:   "https",
 			Host:     "auth.getmondo.co.uk",
-			RawQuery: "client_id=" + clientID + "&redirect_uri=https://pure-oasis-86979.herokuapp.com/auth/callback&response_type=code&state=" + state,
+			RawQuery: "client_id=" + clientID + "&redirect_uri=" + c.Request.Host + "/auth/callback&response_type=code&state=" + state,
 		}
 		c.Redirect(http.StatusTemporaryRedirect, link.String())
 	}
@@ -84,51 +84,80 @@ func setAuthCallbackEndpointWrapper(clientID, clientSecret string) func(c *gin.C
 		var err error
 
 		if authResponse.AuthExpiryTimestamp <= time.Now().Unix() {
+			if authResponse.RefreshToken != "" {
+				form := url.Values{}
+				form.Add("grant_type", "refresh_token")
+				form.Add("client_id", clientID)
+				form.Add("client_secret", clientSecret)
+				form.Add("refresh_token", authResponse.RefreshToken)
 
-			err := c.Request.ParseForm()
-			if err != nil {
-				panic("Failed to parse form")
+				req, err := http.NewRequest("POST", "https://api.monzo.com/oauth2/token", strings.NewReader(form.Encode()))
+				if err != nil {
+					panic("Failed to create a new request")
+				}
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+				resp, err = client.Do(req)
+				if err != nil {
+					panic("Failed to create a new request")
+				}
+
+				defer resp.Body.Close()
+				respBody, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					panic("Failed to create a new request")
+				}
+
+				err = json.Unmarshal(respBody, &authResponse)
+				if err != nil {
+					panic("Failed to unmarshal the response" + err.Error())
+				}
+			} else {
+				err := c.Request.ParseForm()
+				if err != nil {
+					panic("Failed to parse form")
+				}
+
+				authorizationCode := c.Request.Form.Get("code")
+				monzoState := c.Request.Form.Get("state")
+				if monzoState != state {
+					c.JSON(404, gin.H{
+						"Error": "The state does not match, what are you trying to do",
+					})
+					return
+				}
+
+				form := url.Values{}
+				form.Add("grant_type", "authorization_code")
+				form.Add("client_id", clientID)
+				form.Add("client_secret", clientSecret)
+				form.Add("redirect_uri", c.Request.Host+"/auth/callback")
+				form.Add("code", authorizationCode)
+
+				req, err := http.NewRequest("POST", "https://api.monzo.com/oauth2/token", strings.NewReader(form.Encode()))
+				if err != nil {
+					panic("Failed to create a new request")
+				}
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+				resp, err = client.Do(req)
+				if err != nil {
+					panic("Failed to create a new request")
+				}
+
+				defer resp.Body.Close()
+				respBody, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					panic("Failed to create a new request")
+				}
+
+				err = json.Unmarshal(respBody, &authResponse)
+				if err != nil {
+					panic("Failed to unmarshal the response" + err.Error())
+				}
 			}
 
-			authorizationCode := c.Request.Form.Get("code")
-			monzoState := c.Request.Form.Get("state")
-			if monzoState != state {
-				c.JSON(404, gin.H{
-					"Error": "The state does not match, what are you trying to do",
-				})
-				return
-			}
-
-			form := url.Values{}
-			form.Add("grant_type", "authorization_code")
-			form.Add("client_id", clientID)
-			form.Add("client_secret", clientSecret)
-			form.Add("redirect_uri", "https://pure-oasis-86979.herokuapp.com/auth/callback")
-			form.Add("code", authorizationCode)
-
-			req, err := http.NewRequest("POST", "https://api.monzo.com/oauth2/token", strings.NewReader(form.Encode()))
-			if err != nil {
-				panic("Failed to create a new request")
-			}
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-			resp, err = client.Do(req)
-			if err != nil {
-				panic("Failed to create a new request")
-			}
-
-			defer resp.Body.Close()
-			respBody, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				panic("Failed to create a new request")
-			}
-
-			err = json.Unmarshal(respBody, &authResponse)
-			if err != nil {
-				panic("Failed to unmarshal the response" + err.Error())
-			}
 			authResponse.AuthExpiryTimestamp = time.Now().Unix() + int64(authResponse.ExpiresIn)
-
 		}
 
 		req, err := http.NewRequest("GET", "https://api.monzo.com/ping/whoami", nil)
